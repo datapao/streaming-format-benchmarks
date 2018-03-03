15647bda70aa2834c13eb85520c7262d075390c7
Zoltan C. Toth <zoltan@datapao.com>
2018-03-03 16:02:13 +0100

    first shoot for go benchmarks

--- processing-in-python/app.py
+++ processing-in-python/app.py
@@ -1,4 +1,5 @@
 import json
+import ujson
 import avro
 import timeit
 import os
@@ -8,7 +9,7 @@ from avro.io import DatumWriter, DatumReader
 
 event_str = '{"scan_rate": 100, "node_id": "factory.unit.source", "value": 376.9000000000172, "type": "VariantType.Double", "timestamp": "2018-02-25T13:52:33.580957"}'
 event = json.loads(event_str)
-n = 1*100*100
+n = 1*500*1000
 
 print("Starting benchmark...")
 print("Sample event: {}".format(event_str))
@@ -21,16 +22,32 @@ with open(json_filename, "w") as json_f:
         json_f.write(json.dumps(event) + "\n")
 
     json_t = timeit.timeit(write_json, number=n)
-    print("python,json,encoding,{:.2f}".format(n, json_t))
+    print("python,json,encoding,{},{:.2f}".format(n, json_t))
 
 with open(json_filename, "r") as json_f:
     def read_json():
         s = json_f.readline()
         json.loads(s)
     json_wt = timeit.timeit(read_json, number=n)
-    print("python,json,decoding,{:.2f}".format(n, json_wt))
+    print("python,json,decoding,{},{:.2f}".format(n, json_wt))
+
+with open(json_filename, "w") as json_f:
+    def write_ujson():
+        json_f.write(ujson.dumps(event) + "\n")
+
+    json_t = timeit.timeit(write_ujson, number=n)
+    print("python,ujson,encoding,{},{:.2f}".format(n, json_t))
+
+
+with open(json_filename, "r") as json_f:
+    def read_ujson():
+        s = json_f.readline()
+        json.loads(s)
+    json_wt = timeit.timeit(read_ujson, number=n)
+    print("python,ujson,decoding,{},{:.2f}".format(n, json_wt))
 
 print("JSON output size: {:,} bytes".format(os.path.getsize(json_filename)))
+
 
 with open(avro_filename, "wb") as avro_f:
     def write_avro():
@@ -39,7 +56,7 @@ with open(avro_filename, "wb") as avro_f:
     avro_writer = DataFileWriter(avro_f, DatumWriter(), avro_schema)
     avro_t = timeit.timeit(write_avro, number=n)
 
-    print("python,avro,encoding,{:.2f}".format(n, avro_t))
+    print("python,avro,encoding,{},{:.2f}".format(n, avro_t))
     avro_writer.close()
 
 with open(avro_filename, "rb") as avro_f:
@@ -47,7 +64,7 @@ with open(avro_filename, "rb") as avro_f:
         u = reader.__iter__().__next__()
     reader = DataFileReader(avro_f, DatumReader())
     avro_wt = timeit.timeit(read_avro, number=n)
-    print("python,avro,decoding,{:.2f}".format(n, avro_t))
+    print("python,avro,decoding,{},{:.2f}".format(n, avro_wt))
     reader.close()
 
 print("AVRO output size: {:,} bytes".format(os.path.getsize(avro_filename)))
